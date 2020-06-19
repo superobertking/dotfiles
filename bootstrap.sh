e18cbb5485261b484554fe5f9acf0e13ff525a4d
robertking <superobertking@icloud.com>
2020-06-20 07:59:46 +0800

    Add git settings

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,9 +28,28 @@ function alacritty_conf() {
     ln -s $PWD/config/alacritty/${name}.yml ~/.config/alacritty/alacritty.yml
 }
 
+# Git
+function git_conf() {
+    git config --global core.editor nvim
+    git config --global merge.conflictstyle diff3
+    git config --global mergetool.vimdiff.path $(which nvim)
+    git config --global merge.tool 'vimdiff'
+    git config --global commit.gpgsign true
+    git config --global user.signingkey 3DBB35F87E3E0A51
+    # git config --global https.proxy socks5h://127.0.0.1:1080
+    git config --global user.email superobertking@icloud.com
+    git config --global user.name robertking
+    git config --global core.excludesfile ~/.gitignore_global
+    echo ".DS_Store" >> ~/.gitignore_global
+
+    # Useful commands:
+    # git rebase --exec
+}
+
 function main() {
     fish_conf
     nvim_conf
+    git_conf
     alacritty_conf
 }
 
